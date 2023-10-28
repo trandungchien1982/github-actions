375e6ea4372a5a90ab0833301820e2de52206b3c
tdc <37946487+tdc@users.noreply.github.com>
2023-10-28 20:38:55 +0700

    Add files to [02.MultipleStagesDocker]

--- hello-world/hello-script.sh
+++
@@ -1,2 +0,0 @@
-echo 'Hello World from GitHub Actions!'
-echo 'Welcome to the wonderful land + Good luck! ...'
---
+++ multi-stages-docker/hello-script.sh
@@ -0,0 +1,2 @@
+echo 'Hello World from GitHub Actions!'
+echo 'Welcome to the Docker Envs for running scripts :) ...'
