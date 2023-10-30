de83d7956be6e9efe980fc6a75e8cb66cb6da438
tdc <37946487+tdc@users.noreply.github.com>
2023-10-30 13:04:35 +0700

    Add files to [03.Pineline4Java]

---
+++ gha-ci-java/simple-script.sh
@@ -0,0 +1,2 @@
+echo 'Hello! This is the content of script in GitHub Actions for Java ... '
+echo 'Welcome to the CI/CD world ...'
--- multi-stages-docker/hello-script.sh
+++
@@ -1,2 +0,0 @@
-echo 'Hello World from GitHub Actions!'
-echo 'Welcome to the Docker Envs for running scripts :) ...'
