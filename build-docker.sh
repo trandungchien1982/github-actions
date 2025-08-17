b224b452a03b4a7b2eeabb8aae003a648ea38a07
Trần Dũng Chiến <37946487+trandungchien1982@users.noreply.github.com>
2025-08-17 13:47:28 +0700

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -17,6 +17,8 @@ docker build ./hello-world-app -t "$IMAGE_NAME_GLOBAL"
 
 echo Push new image to Docker Registry
 docker images
+
+docker logout
 docker login -u "$LOGIN_DOCKER_USR" -p "$LOGIN_DOCKER_PWD"
 docker push "$IMAGE_NAME_GLOBAL"
 
